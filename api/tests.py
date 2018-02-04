6d5a919c0c7efd7f042348a1fd43d46f355d52d9
spriglet <redfroglet@gmail.com>
2018-02-03 18:41:07 -0600

    This is the first commit of the project

--- api/tests.py
+++ api/tests.py
@@ -2,6 +2,10 @@
 
 from django.test import TestCase
 from .models import Lesson
+from rest_framework.test import APIClient
+from rest_framework import status
+from django.urls import reverse
+
 
 # Create your tests here.
 class ModelTestCase(TestCase):
@@ -18,6 +22,22 @@ class ModelTestCase(TestCase):
         new_count = Lesson.objects.count()
         self.assertNotEqual(old_count, new_count)
 
+# Define this after the ModelTestCase
+class ViewTestCase(TestCase):
+    """Test suite for the api views."""
+
+    def setUp(self):
+        """Define the test client and other test variables."""
+        self.client = APIClient()
+        self.lesson_data = {'name': 'Japanese Class' , 'description':'Join to learn Japanese'}
+        self.response = self.client.post(
+            reverse('create'),
+            self.lesson_data,
+            format="json")
+
+    def test_api_can_create_a_lesson(self):
+        """Test the api has bucket creation capability."""
+        self.assertEqual(self.response.status_code, status.HTTP_201_CREATED)
 
 
 
--- api/views.py
+++ api/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+# api/views.py
 
-# Create your views here.
+from rest_framework import generics
+from .serializers import LessonSerializer
+from .models import Lesson
+
+class CreateView(generics.ListCreateAPIView):
+    """This class defines the create behavior of our rest api."""
+    queryset = Lesson.objects.all()
+    serializer_class = LessonSerializer
+
+    def perform_create(self, serializer):
+        """Save the post data when creating a new bucketlist."""
+        serializer.save()
