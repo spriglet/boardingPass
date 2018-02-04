8955dd756bb58424ce3ae3864054f844ead498d8
spriglet <redfroglet@gmail.com>
2018-02-03 21:34:28 -0600

    This commit was created after adding create,delete,update and view to the rest api. 02/03/2018

--- api/tests.py
+++ api/tests.py
@@ -36,8 +36,36 @@ class ViewTestCase(TestCase):
             format="json")
 
     def test_api_can_create_a_lesson(self):
-        """Test the api has bucket creation capability."""
+        """Test the api has lesson creation capability."""
         self.assertEqual(self.response.status_code, status.HTTP_201_CREATED)
 
+    def test_api_can_get_a_lesson(self):
+        """Test the api can get a given bucketlist."""
+        lesson = Lesson.objects.get()
+        response = self.client.get(
+            reverse('details',
+                    kwargs={'pk': lesson.id}), format="json")
 
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertContains(response, lesson)
+
+    def test_api_can_update_lesson(self):
+        """Test the api can update a given lesson."""
+        lesson = Lesson.objects.get()
+        change_lesson = {'name': 'Something new','description':'New description'}
+        res = self.client.put(
+            reverse('details', kwargs={'pk': lesson.id}),
+            change_lesson, format='json'
+        )
+        self.assertEqual(res.status_code, status.HTTP_200_OK)
+
+    def test_api_can_delete_lesson(self):
+        """Test the api can delete a lesson."""
+        lesson = Lesson.objects.get()
+        response = self.client.delete(
+            reverse('details', kwargs={'pk': lesson.id}),
+            format='json',
+            follow=True)
+
+        self.assertEquals(response.status_code, status.HTTP_204_NO_CONTENT)
 
--- api/urls.py
+++ api/urls.py
@@ -3,9 +3,14 @@
 from django.conf.urls import url, include
 from rest_framework.urlpatterns import format_suffix_patterns
 from .views import CreateView
+from .views import DetailsView
 
 urlpatterns = {
+
     url(r'^lesson/$', CreateView.as_view(), name="create"),
+    url(r'^lesson/(?P<pk>[0-9]+)/$',
+        DetailsView.as_view(), name="details"),
+
 }
 
 urlpatterns = format_suffix_patterns(urlpatterns)
