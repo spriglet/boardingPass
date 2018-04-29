e5ff28d0bfe7611c4c4b31bcd252b1f13800492d
spriglet <redfroglet@gmail.com>
2018-04-29 17:53:08 -0500

    Added authentication to the rest api and front end code. April 15

--- boardingPass/settings.py
+++ boardingPass/settings.py
@@ -162,7 +162,7 @@ STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = [
-    os.path.join(PROJECT_ROOT, 'static'),
+    os.path.join(PROJECT_ROOT, 'staticfiles'),
 ]
 
 # Simplified static file serving.
